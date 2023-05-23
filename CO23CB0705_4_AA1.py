98b4f06077cb78992385a3d7de4fcc2d65c9da38
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-23 17:04:44 +0530

    Update CO23CB0705_4_AA1.py

--- CO23CB0705_4_AA1.py
+++ CO23CB0705_4_AA1.py
@@ -1,1 +1,10 @@
+# Example nested dictionary
+my_dict = {'name': 'John', 'details': {'age': 25, 'city': 'New York'}}
 
+# Updating the nested dictionary with new values
+my_dict['details']['age'] = 13
+my_dict['details']['city'] = 'New town, Avora'
+my_dict['details']['occupation'] = 'Wizard Student'
+
+# Printing the updated nested dictionary
+print(my_dict)
