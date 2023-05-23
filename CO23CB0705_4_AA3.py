f79e5a33e4ee230ff0cf4235423bac7ea7ef75fa
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-23 17:34:04 +0530

    Update CO23CB0705_4_AA3.py

--- CO23CB0705_4_AA3.py
+++ CO23CB0705_4_AA3.py
@@ -1,1 +1,30 @@
+# Create a nested dictionary to store country details
+country_details = {
+    'USA': {
+        'capital': 'Washington, D.C.',
+        'currency': 'United States Dollar (USD)'
+    },
+    'Germany': {
+        'capital': 'Berlin',
+        'currency': 'Euro (EUR)'
+    },
+    'Japan': {
+        'capital': 'Tokyo',
+        'currency': 'Japanese Yen (JPY)'
+    },
+    'Brazil': {
+        'capital': 'Brasília',
+        'currency': 'Brazilian Real (BRL)'
+    },
+    'Australia': {
+        'capital': 'Canberra',
+        'currency': 'Australian Dollar (AUD)'
+    }
+}
 
+# Display the details of each country
+for country, details in country_details.items():
+    print(f"Country: {country}")
+    print(f"Capital: {details['capital']}")
+    print(f"Currency: {details['currency']}")
+    print()
