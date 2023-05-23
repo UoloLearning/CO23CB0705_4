c3f439b0b0c4f448b54a139da08f56c8a7aaddd2
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-23 16:45:05 +0530

    Update CO23CB0705_4_A1.py

--- CO23CB0705_4_A1.py
+++ CO23CB0705_4_A1.py
@@ -1,1 +1,11 @@
+my_dict = {'name': 'Conji', 'age': 13, 'city': 'New town, Avora'}
 
+# Accessing dictionary elements using [] syntax
+name = my_dict['name']
+age = my_dict['age']
+city = my_dict['city']
+
+# Printing the accessed elements
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
